36ed608c93e825fa6fa38f9e5cebd0303dfb8a19
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-10-17 12:01:48 -0300

    agregados5AlumnosBDD

--- src/transeversalmain/TranseversalMain.java
+++ src/transeversalmain/TranseversalMain.java
@@ -1,5 +1,3 @@
-
-
 package transeversalmain;
 
 import java.sql.*;
@@ -7,34 +5,70 @@ import java.time.LocalDate;
 import java.util.List;
 import transeversal.datos.AlumnoData;
 import transeversal.entidades.Alumno;
+
 public class TranseversalMain {
 
-    
     public static void main(String[] args) {
-          
-            int dni = 30200666;
-            String apellido = "Torrez";
-            String nombre = "Gabriel";
-            LocalDate fechaNac = LocalDate.of(2000, 01, 15);
-            boolean estado = true;
-            
-            Alumno a1= new Alumno(dni,apellido,nombre,fechaNac,estado);
-            AlumnoData ad1= new AlumnoData();
-//            ad1.guardarAlumno(a1);
-            Alumno encontrado= ad1.buscarAlumnoPorDni(30200666);
-            System.out.println("alumno encontrado "+encontrado);
-            
-            List<Alumno> listado=ad1.obtenerAlumnos();
-            for (Alumno alumno : listado) {
-                System.out.println(alumno);
-        }
-            encontrado.setNombre("Hugo Gabriel");
-            ad1.modificarAlumno(encontrado);
-            
-            
-            ad1.eliminarAlumno(encontrado.getIdAlumno());
+
+        //ALUMNOS
+        int dni = 30200666;
+        String apellido = "Torrez";
+        String nombre = "Gabriel";
+        LocalDate fechaNac = LocalDate.of(2000, 01, 15);
+        boolean estado = true;
+        Alumno a1 = new Alumno(dni, apellido, nombre, fechaNac, estado);
+        AlumnoData ad1 = new AlumnoData();
+            ad1.guardarAlumno(a1);
+//      Alumno encontrado = ad1.buscarAlumnoPorDni(30200666);
+//        System.out.println("alumno encontrado " + encontrado);
+//
+//        List<Alumno> listado = ad1.obtenerAlumnos();
+//        for (Alumno alumno : listado) {
+//            System.out.println(alumno);
+//        }
+//        encontrado.setNombre("Hugo Gabriel");
+//        ad1.modificarAlumno(encontrado);
+//
+//        ad1.eliminarAlumno(encontrado.getIdAlumno());
+
         
-            
+        int dni2 = 32465789 ;
+        String apellido2 = "Sbodio";
+        String nombre2 = "Mateo";
+        LocalDate fechaNac2 = LocalDate.of(1995, 04, 01);
+        boolean estado2 = true;
+        
+        Alumno a2 = new Alumno(dni2, apellido2, nombre2, fechaNac2, estado2);
+        ad1.guardarAlumno(a2);
+        
+        int dni3 = 33989017 ;
+        String apellido3 = "Rios";
+        String nombre3 = "Julian";
+        LocalDate fechaNac3 = LocalDate.of(1990, 02, 10);
+        boolean estado3 = true;
+        
+        Alumno a3 = new Alumno(dni3, apellido3, nombre3, fechaNac3, estado3);
+        ad1.guardarAlumno(a3);
+        
+        int dni4 = 38657365 ;
+        String apellido4 = "Suarez";
+        String nombre4 = "Martin";
+        LocalDate fechaNac4 = LocalDate.of(1999, 07, 15);
+        boolean estado4 = true;
+        
+        Alumno a4 = new Alumno(dni4, apellido4, nombre4, fechaNac4, estado4);
+        ad1.guardarAlumno(a4);
+        
+        int dni5 = 35435365 ;
+        String apellido5 = "Sturniolo";
+        String nombre5 = "Bruno";
+        LocalDate fechaNac5 = LocalDate.of(1992, 03, 17);
+        boolean estado5 = true;
+        
+        Alumno a5 = new Alumno(dni5, apellido5, nombre5, fechaNac5, estado5);
+        ad1.guardarAlumno(a5);
+
+        
 //            ps.setInt(1, dni);
 //            ps.setString(2, apellido);
 //            ps.setString(3, nombre);
@@ -158,8 +192,6 @@ public class TranseversalMain {
 //        } catch (SQLException e) {
 //            System.err.println("Error: " + e.getMessage());
 //        }
-
     }
-    
+
 }
-    
